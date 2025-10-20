6f944da66705f3cf7480c3615f77baaedacb1ad0
KR <238293813+KittyRx@users.noreply.github.com>
2025-10-20 22:13:35 +0200

    Add Theory.cpp with namespaces and templates examples
    This file introduces namespaces, user input handling, type definitions, and function overloading in C++. It also demonstrates the use of templates and constexpr for compile-time checks.

---
+++ Theory.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <string>
+
+//Namespace (allows reusing of entities with same name by using accesing namespaces; kinda wraping them in an object).
+namespace alpha {
+	int value = 5;
+};
+
+namespace beta {
+	int value = 10;
+};
+
+int exampleNamespace() {
+	int value = 2;
+
+	//Not declaring namespace will use local value.
+	std::cout << "Printed value: " << value << endl;
+
+	//but declaring namespace will use that namespace's value.
+	std::cout << "Printed value: " << alpha::value << endl;
+
+	// '::' scope resolution operator; kind of like '.something' in object accessing .
+	std::string input;
+
+	// User input assigned to the input variable.
+	std::cin >> input;
+
+	// After the user inputs wrong data, we should use "clear" to reset the error flags on input and continue reading.
+	std::cin.clear();
+
+	/***
+	For longer strings "getline" can be used, if "cin" is preceeding "getline" the "\n" character after user input will be passed on to the second cin inside "getline" user input because of buffering.
+	This happens because "cin" reads until the white space character and does not consume the "\n" token at the end which ends up being buffered to the next input operation.
+	( Unlike "cin" , "getline" consumes the new line character at the end of the input ).
+	To avoid that we can use "ws" to clear the buffer of whitespace characters, before checking for new user input.
+	***/
+	std::getline(std::cin >> std::ws, input);
+
+	//For further sanitization we use "ignore" to clear the buffer of unwanted characters. The 2 parameters are number of characters to flush OR everything until "\n" is found, hopefuly at the end of the input.
+	std::cin.ignore(10000, '\n');
+
+	//And for an even clearer way instead of using an arbitrary number we can define the size of the numeric limits to the maximum stream size, which means everything in the buffer.
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+	//The "fflush" command forces "cout" to push whatever is inside the buffer before doing anything else. Not recommended for INPUT sanitization.
+	fflush(stdout);
+
+	//For OUTPUT sanitization we can simply use "endl" to add a new line and flush the buffer.
+	std::cout << "output" << std::endl;
+
+	return 0;
+};
+
+//Can also declare what "namespace" is used for definitions. Loading the prefix|package before its useage which is later infered by what members are accessed.
+using namespace std;
+
+//Or, preferably to avoid naming conflicts and unnecessary renaming or memory overhead, 
+// We can load a specific member of the "namespace" akin to accessing a json file.
+using std::string;
+
+//The "typedef" is used to rename existing data types or give them "nicknames"; so here "type_standard" of string is renamed to "str_t"; t used to indicate its a type.
+typedef std::string strTypeDef_t;
+
+//But the common standard is now changed to "using" for type renaming. Because it also works with templates.
+using strUsing_t = std::string;
+
+int main() {
+	//So here all denominationg represent the same type.
+	string fluff1 = "fluff";
+	strTypeDef_t fluff2 = "fluff";
+	strUsing_t fluff3 = "fluff";
+	return 0;
+}
+
+//As opposed to TS, where you can declare different optional arguments to overload a function, the cpp overload is:
+void function(int argument) {};
+void function(string argument) {};
+void function(double argument) {};
+
+//But, instead of declaring it like 3 different functions with the same name but different signatures ( parameters + name ), alternatively we can use templates.
+template<typename T>
+
+// Here "T" is fulfilling the role of a placeholder for the type of argument passed to the function.
+void function(T argument) {
+
+	/***
+   "constexpr" tells the compiler to only compile if something matches the if condition. Meaning it can clean overhead at compile time but can break if used with runtime data checks.
+	We then access the type comparison "is_same" and pass parameters of T (type of passed argument) with the expected type.
+	And it compare the result and returns a boolean value we access with "_v".
+	***/
+	if constexpr (std::is_same_v < T, typeExpected) {
+		return void doLogicForOneType();
+	}
+	else {
+		return void doLogicForAnotherType();
+	}
+}
+
+//Or for multiple types we can use several "typename", since "T" is instanced for local scope of each function ( T is different for each different function ), 
+//but not instanced per variable, where ( argument1, argument2 ) might be different types.
+template<typename S, typename U, typename V>
+void function(S argument1, U argument2, V argument3) {
+	//And then run comparison and logic based on types inside.
+}
