199c16092d2725e3b5b415f8204e10c0f1b3fe26
KittyRx <238293813+KittyRx@users.noreply.github.com>
2025-10-23 23:31:54 +0200

    Moved from VS to VSC for ease of use, fixed some mistakes in theory, and added new file

---
+++ Questionaire.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+	int score = 0;
+
+	// First we define questions.
+	string questions[] = {
+		"What is the capital of France?",
+		"What is 2 + 2?",
+		"What is the largest planet in our solar system?"};
+
+	// Define the size of questions array.
+	int questionsCount = sizeof(questions) / sizeof(string);
+
+	// Define the correct answers.
+	string answers[] = {
+		"Paris",
+		"4",
+		"Jupiter"};
+
+	// Then we define options for answers.
+	string answerOptions[3][4] = {
+		{"Paris", "London", "Berlin", "Madrid"},
+		{"3", "5", "4", "22"},
+		{"Earth", "Mars", "Saturn", "Jupiter"}};
+
+	int answersSize = sizeof(answerOptions) / sizeof(string);
+
+	// We create a simple display for the quizz.
+	cout << "Quizz Time!" << endl;
+	cout << "Answer the following questions by entering the number corresponding to your answer." << endl;
+
+	// Loop to display all questions.
+	for (int i = 0; i < questionsCount; i++)
+	{
+		cout << questions[i] << endl;
+
+		// And all answers for each question.
+		for (string answer : answerOptions[i])
+		{
+			cout << " - " << answer << endl;
+		};
+
+		// Get user answer for each question.
+		string reply;
+		cout << "Your answer:" << endl;
+		getline(cin, reply);
+
+		// Empty answers loop.
+		while (reply.empty())
+		{
+			// Create a skip for those who don't want to answer.
+			cout << "Would you like to skip this question(yes/y | no/n) ?" << endl;
+			cout << "Your choice:" << endl;
+			getline(cin >> ws, reply);
+			// If yes next question.
+			if (reply == "yes" || reply == "y")
+			{
+				break;
+			}
+			// If no ask the question again.
+			else
+			{
+				cout << questions[i] << endl;
+				cout << "Your answer:" << endl;
+				getline(cin >> ws, reply);
+			};
+		};
+
+		// Then check if the answers are correct.
+		if (reply != answers[i])
+		{
+			score -= 1;
+		}
+		else
+		{
+			score += 1;
+		};
+	};
+
+	// Output results:
+	cout << "Your score is : " << score << " out of " << questionsCount << endl;
+
+	return 0;
+};
--- Theory.cpp
+++ Theory.cpp
@@ -7,26 +7,29 @@ Example functions demonstating various C++ features.The nested functions are tur
 This is also writen in a ".cpp" file to indicate the language, make use of the C++ standard library and show the syntax highlighting as well as the correction of code snippets.
 ***/
 
-void exampleNamespace() {
+// Namespace (allows reusing of entities with same name by using accesing namespaces; kinda wraping them in an object).
+namespace alpha
+{
+	int value = 5;
+};
 
-	//Namespace (allows reusing of entities with same name by using accesing namespaces; kinda wraping them in an object).
-	namespace alpha {
-		int value = 5;
-	};
+namespace beta
+{
+	int value = 10;
+};
 
-	namespace beta {
-		int value = 10;
-	};
+void exampleNamespace()
+{
 
 	int value = 2;
 
-	//Not declaring namespace will use "local" value.
+	// Not declaring namespace will use "local" value.
 	std::cout << "Printed value: " << value << std::endl;
 
-	//But declaring namespace will use that "namespace's value".
+	// But declaring namespace will use that "namespace's value".
 	std::cout << "Printed value: " << alpha::value << std::endl;
 
-	//Can also declare what "namespace" is used for definitions. Loading the prefix|package before its useage which is later infered by what members are accessed.
+	// Can also declare what "namespace" is used for definitions. Loading the prefix|package before its useage which is later infered by what members are accessed.
 	using namespace std;
 
 	/***
@@ -34,10 +37,10 @@ void exampleNamespace() {
 	We can load a specific member of the "namespace" akin to accessing a json file.
 	***/
 	using std::string;
-
 };
 
-void exampleTypeRenaming() {
+void exampleTypeRenaming()
+{
 
 	/***
 	The "typedef" is used to rename existing data types or give them "nicknames";
@@ -45,17 +48,17 @@ void exampleTypeRenaming() {
 	***/
 	typedef std::string strTypeDef_t;
 
-	//But the common way of doing it has now changed to "using" for type renaming. Because it also works with templates.
+	// But the common way of doing it has now changed to "using" for type renaming. Because it also works with templates.
 	using strUsing_t = std::string;
 
-	//So here all denominations represent the same type.
+	// So here all denominations represent the same type.
 	std::string fluff1 = "fluff";
 	strTypeDef_t fluff2 = "fluff";
 	strUsing_t fluff3 = "fluff";
-
 };
 
-void exampleInputAndOutput() {
+void exampleInputAndOutput()
+{
 
 	// '::' scope resolution operator; kind of like '.something' in object accessing .
 	std::string input;
@@ -74,29 +77,29 @@ void exampleInputAndOutput() {
 	***/
 	std::getline(std::cin >> std::ws, input);
 
-	//For further sanitization we use "ignore" to clear the buffer of unwanted characters. The 2 parameters are number of characters to flush OR everything until "\n" is found, hopefuly at the end of the input.
+	// For further sanitization we use "ignore" to clear the buffer of unwanted characters. The 2 parameters are number of characters to flush OR everything until "\n" is found, hopefuly at the end of the input.
 	std::cin.ignore(10000, '\n');
 
-	//And for an even clearer way instead of using an arbitrary number we can define the size of the numeric limits to the maximum stream size, which means everything in the buffer.
+	// And for an even clearer way instead of using an arbitrary number we can define the size of the numeric limits to the maximum stream size, which means everything in the buffer.
 	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-	//The "fflush" command forces "cout" to push whatever is inside the buffer before doing anything else. Not recommended for INPUT sanitization.
+	// The "fflush" command forces "cout" to push whatever is inside the buffer before doing anything else. Not recommended for INPUT sanitization.
 	std::fflush(stdout);
 
-	//For OUTPUT sanitization we can simply use "endl" to add a new line and flush the buffer.
+	// For OUTPUT sanitization we can simply use "endl" to add a new line and flush the buffer.
 	std::cout << "output" << std::endl;
-
 };
 
-void exampleOverloadingAndTemplates() {
+// But, instead of declaring it like 3 different functions with the same name but different signatures (  name + parameters  ), we can use templates.
+template <typename T>
 
-	//As opposed to TS, where you can declare different optional arguments to overload a function, the cpp overload is:
+void exampleOverloadingAndTemplates()
+{
+
+	// As opposed to TS, where you can declare different optional arguments to overload a function, the cpp overload is:
 	std::string example = "int function(int argument) {}";
 	std::string example = "std::string function(std::string argument) {}";
 	std::string example = "double function(double argument) {}";
-
-	//But, instead of declaring it like 3 different functions with the same name but different signatures (  name + parameters  ), we can use templates.
-	template<typename T>
 
 	/***
 	Here "T" is fulfilling the role of a placeholder for the type of argument passed to the function.
@@ -104,8 +107,9 @@ void exampleOverloadingAndTemplates() {
 	***/
 	std::string example = "dataType function(T argument) {}";
 
-	//We can either do type comparisons without defining when the type is known, be it at compile time or runtime.
-	if (std::is_same_v< T, int typeExpected>) {
+	// We can either do type comparisons without defining when the type is known, be it at compile time or runtime.
+	if (std::is_same_v<T, int typeExpected>)
+	{
 		std::string example = "return dataType Logic";
 	};
 
@@ -113,7 +117,8 @@ void exampleOverloadingAndTemplates() {
 	Or we can use "constexpr" and tell the compiler to only compile if something matches the if condition.
 	Meaning it can clean overhead at compile time but can break if used with runtime data checks.
 	***/
-	if constexpr (std::is_same_v< T, int typeExpected>) {
+	if constexpr (std::is_same_v<T, int typeExpected>)
+	{
 		std::string example = "return dataType Logic";
 	}
 
@@ -126,58 +131,61 @@ void exampleOverloadingAndTemplates() {
 	Or for multiple types we can use several "typename", since "T" is instanced for local scope of each function (T is different for each different function),
 	but not instanced per variable, where ( argument1, argument2 ) might be different types.
 	***/
-	template<typename S, typename U, typename V>
+	template <typename S, typename U, typename V>
 	std::string example = "dataType function(S argument1, U argument2, V argument3) {}";
 
-	//And then run comparison and logic based on types inside.
-	if (std::is_same_v< S, int typeExpected>) {
+	// And then run comparison and logic based on types inside.
+	if (std::is_same_v<S, int typeExpected>)
+	{
 		std::string example = "return Logic";
 	};
 
 	// There is also the alternative of using "auto" for type deduction, but it is compile deduced and not as flexible, its basically lazy typing and slightly safer than using "any" in typescript.
 	auto variable = randomType();
-
 };
 
-void exampleArrays() {
+void exampleArrays()
+{
 
-	//Arrays can be declared statically or dynamically. And the array can be initialized with values or they can be added later.
-	int arrayStaticValues[5] = { 1,2,3,4,5 };
+	// Arrays can be declared statically or dynamically. And the array can be initialized with values or they can be added later.
+	int arrayStaticValues[5] = {1, 2, 3, 4, 5};
 
-	//Declaring values after initializing array with set size.
-	int arrayStaticNoValues[5];
-	int arrayStaticNoValues = { 1,2,3,4,5 };
+	// Declaring values after initializing array with set size.
+	int arrayStaticNoValues[5] = {};
+	int arrayStaticNoValues[5] = {1, 2, 3, 4, 5};
 
-	//Dynamic arrays are allocated memory on the heap at runtime and require dealocation.
+	// Dynamic arrays are allocated memory on the heap at runtime and require dealocation.
 	int n = std::cin.get();
-	int* arrayDynamic = new int[n];
+	int *arrayDynamic = new int[n];
 
-	//To avoid manual memory management use of smart pointers is recommended.
+	// To avoid manual memory management use of smart pointers is recommended.
 	std::unique_ptr<int[]> arrayName(new int[n]);
 
-	//Can use "sizeof" to determine the size of array in bytes.
+	// Can use "sizeof" to determine the size of array in bytes.
 	sizeof(arrayStaticValues);
 
-	//This can also be used to determine the number of elements in a static array.
-	std::string strings[] = { "one","two","three" };
+	// This can also be used to determine the number of elements in a static array.
+	std::string strings[] = {"one", "two", "three"};
 	sizeof(strings) / sizeof(std::string);
-	sizeof(strings) / sizeof(names[0]);
+	sizeof(strings) / sizeof(strings[0]);
 
-	//When passing an array to a function it decays to a pointer so the sizeof does not work as expected.
+	// When passing an array to a function it decays to a pointer so the sizeof does not work as expected.
 	std::string example = "std::string function(std::string strings[]){}";
 
-	//Minimal detail I never use when looping
-	for (std::string string : strings) {};
-
-	//Arrays can also be multi dimensional, akin to matrixes.
-	std::string elements[3][3] = {
-		{"one","two","three"},
-		{"four","five","six"},
-		{"seven","eight","nine"}
+	// Minimal detail I never use when looping
+	for (std::string string : strings)
+	{
 	};
 
-	//Which can be used similarly to an ECS system for entity component storage or movement logic.
-	enum ComponentIndex {
+	// Arrays can also be multi dimensional, akin to matrixes.
+	std::string elements[3][3] = {
+		{"one", "two", "three"},
+		{"four", "five", "six"},
+		{"seven", "eight", "nine"}};
+
+	// Which can be used similarly to an ECS system for entity component storage or movement logic.
+	enum ComponentIndex
+	{
 		POS_X,
 		POS_Y,
 		POS_Z,
@@ -187,25 +195,25 @@ void exampleArrays() {
 		COUNT
 	};
 
-	//We might want to also declare this dynamically later on but for now its a static value to showcase the concept.
-	const ENTITY_COUNT = 2;
+	// We might want to also declare this dynamically later on but for now its a static value to showcase the concept.
+	const int ENTITY_COUNT = 2;
 
-	//We can either declare "component_count" or use the safer "count" method and access it as a member of the enum.
+	// We can either declare "component_count" or use the safer "count" method and access it as a member of the enum.
 	float movementLogic[ENTITY_COUNT][ComponentIndex::COUNT] = {
-		{1.0f, 2.0f, 3.0f, 0.1f, 0.2f, 0.3f},  // Entity 0
-		{4.0f, 5.0f, 6.0f, 0.4f, 0.5f, 0.6f},  //Entity 1
+		{1.0f, 2.0f, 3.0f, 0.1f, 0.2f, 0.3f}, // Entity 0
+		{4.0f, 5.0f, 6.0f, 0.4f, 0.5f, 0.6f}, // Entity 1
 	};
 
-	//So here accesing position of entity 1 on the Y axis would be:
-	float entity1POS_Y = movementLogic[1][ComponentIndex::POS_Y]; //Outputs 5.0f
+	// So here accesing position of entity 1 on the Y axis would be:
+	float entity1POS_Y = movementLogic[1][ComponentIndex::POS_Y]; // Outputs 5.0f
 
-	//And if we really wanted to create a key value sort of system in multidimensional arrays we can always just use 2 enums.
-	enum entityIndex {
+	// And if we really wanted to create a key value sort of system in multidimensional arrays we can always just use 2 enums.
+	enum entityIndex
+	{
 		ENTITY_0,
 		ENTITY_1,
 	};
 
-	//And then access it accordingly.
-	float entity0POS_Z = movementLogic[entityIndex::ENTITY_0][ComponentIndex::POS_Z]; //Outputs 3.0f
-
+	// And then access it accordingly.
+	float entity0POS_Z = movementLogic[entityIndex::ENTITY_0][ComponentIndex::POS_Z]; // Outputs 3.0f
 };
